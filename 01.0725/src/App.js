097bd1f32683d24f3a7a37a77231c7f81c328854
anemoa <anemoia.yves@gmail.com>
2024-07-25 13:44:55 +0900

    study: react component 생성

--- 01.0725/src/App.js
+++ 01.0725/src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(){
+  return (
+    <div>
+      <header>
+        <h1><a href="/">React</a></h1>
+      </header>
+    </div>
+  );
+}
+
+function Nav(){
+  return(
+    <div>
+      <nav>
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/2">css</a></li>
+          <li><a href="/read/3">js</a></li>
+        </ol>
+      </nav>
+    </div>
+  );
+}
+
+function Article(){
+  return(
+    <div>
+      <article>
+        <h2>Welcome</h2>
+        Hello, Web
+      </article>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      Hello React!!
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
