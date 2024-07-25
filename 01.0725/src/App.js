120fda78ebee79659769f5fd0668c9cce2aa599a
anemoa <anemoia.yves@gmail.com>
2024-07-25 14:16:17 +0900

    study: react props 활용

--- 01.0725/src/App.js
+++ 01.0725/src/App.js
@@ -1,47 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header(){
+function Header(props){
+  console.log('props', props.title);
   return (
     <div>
       <header>
-        <h1><a href="/">React</a></h1>
+        <h1><a href="/">{props.title}</a></h1>
       </header>
     </div>
   );
 }
 
-function Nav(){
+function Nav(props){
+  // const lis = [
+  //   <li><a href="/read/1">html2</a></li>,
+  //   <li><a href="/read/2">css2</a></li>,
+  //   <li><a href="/read/3">js2</a></li>
+  // ];
+
+  const lis = [];
+  for(let i = 0; i < props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>)
+  }
+
   return(
     <div>
       <nav>
         <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
-          <li><a href="/read/3">js</a></li>
+          {lis}
         </ol>
       </nav>
     </div>
   );
 }
 
-function Article(){
+function Article(props){
   return(
     <div>
       <article>
-        <h2>Welcome</h2>
-        Hello, Web
+        <h2>{props.title}</h2>
+        {props.body}
       </article>
     </div>
   );
 }
 
 function App() {
+  const topics = [
+    {id: 1, title: 'html', body: 'html is...'},
+    {id: 2, title: 'css', body: 'css is...'},
+    {id: 3, title: 'js', body: 'js is...'},
+  ];
+
   return (
     <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="WEB"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcom" body="Hello, Web"></Article>
+
     </div>
   );
 }
