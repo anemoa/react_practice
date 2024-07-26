f0d9b7f3e51412ee757fb1307d44457db54f9f65
anemoa <anemoia.yves@gmail.com>
2024-07-26 16:56:35 +0900

    study: react DELETE

--- 01.0725/src/App.js
+++ 01.0725/src/App.js
@@ -153,10 +153,24 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
-    contextControll = <li><a href={'/Update/' + id} onClick={event => {
+    contextControll = <>
+      <li><a href={'/Update/' + id} onClick={event => {
       event.preventDefault();
       setMode('UPDATE');
-    } }>Update</a></li>;
+      }}>Update</a></li>
+      <li>
+        <input type="button" value='Delete' onClick={() => {
+          const newTopics = [];
+          for(let i = 0 ; i < topics.length; i++){
+            if(topics[i].id !== id){
+              newTopics.push(topics[i])
+            }
+          }
+          setTopics(newTopics);
+          setMode('WELCOME');
+        }}/>
+      </li>
+    </>
   } else if (mode === 'CREATE') {
     content = (
       <Create
