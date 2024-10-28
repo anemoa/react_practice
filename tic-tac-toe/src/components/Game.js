6d67b7d83c5015e4cb650df9007bace4379c88d4
anemoa <anemoia.yves@gmail.com>
2024-10-28 20:10:14 +0900

    study: tic-tac-toe game added components

---
+++ tic-tac-toe/src/components/Game.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Board from './Board'
+
+const Game = () => {
+
+	const handleClick = () => {
+
+	}
+
+	const jumpTo = () => {
+
+	}
+
+	const renderMoves = () => {
+		
+	}
+
+
+
+  return (
+	<div>
+		<Board onClick={handleClick}/>
+	</div>
+  )
+}
+
+export default Game
