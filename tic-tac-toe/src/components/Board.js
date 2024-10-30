b7d61136ca795a73f177d7730992f574cc0e8653
anemoa <anemoia.yves@gmail.com>
2024-10-30 22:49:41 +0900

    study: tic-tac-toe game added style

--- tic-tac-toe/src/components/Board.js
+++ tic-tac-toe/src/components/Board.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Square from './Square'
 
+const boardStyle = {
+    border: "4px solid darkblue",
+    borderRadius: "10px",
+    width: "250px",
+    height: "250px",
+    margin: "0 auto",
+    display: "grid",
+    gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)",
+};
 
 
 const Board = ({squares, onClick}) => {
   return (
-	<div>
+	<div style={boardStyle}>
 		<Square value="1" onClick={() => onClick("dummy value")} />
 		<Square value="2" onClick={() => onClick("dummy value")} />
 		<Square value="3" onClick={() => onClick("dummy value")} />
@@ -19,4 +28,4 @@ const Board = ({squares, onClick}) => {
   )
 }
 
-export default Board
+export default Board;
--- tic-tac-toe/src/components/Square.js
+++ tic-tac-toe/src/components/Square.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-
+const btnStyle = {
+    background: "lightblue",
+    border: "2px solid darkblue",
+    fontSize: "30px",
+    fontWeight: "800",
+    cursor: "pointer",
+    outline: "none",
+};
 
 const Square = ({value, onClick}) => {
 	return (
-		<button onClick={onClick}>
+		<button style={btnStyle} onClick={onClick}>
 			{value}
 		</button>
 	)
