b24e34067c0b9d1e4259623f49f222d482780f72
anemoa <anemoia.yves@gmail.com>
2024-11-02 23:34:31 +0900

    study: tic-tac-toe game added useState hook

--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import Board from './Board'
+import { useState } from 'react';
+import { calculateWinner } from '../helpers';
 
 const Game = () => {
+
+	const [board, setBoard] = useState(Array(9).fill(null));
+	const [xIsNext, setXisNext] = useState(true);
+	const winner = calculateWinner(board);
 
 	const handleClick = () => {
 
