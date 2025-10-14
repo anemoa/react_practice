980ef871c3ce0baaf24f364a6c93c9f46df7abf6
anemoa <anemoia.yves@gmail.com>
2025-10-14 23:46:00 +0900

    study: Starting with TypeScript basics

---
+++ 09.typescript/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+    const name: string = 'yves';
+    const age: number = 22;
+
+    return (
+        <>
+            <h1>헬로 월드</h1>
+			<p> 이름: {name} </p>
+			<p> 나이: {age}살 </p>
+        </>
+    );
+}
+
+export default App;
