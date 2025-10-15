fc61452841ab9b07be24eca43913ec2eae6e4a12
anemoa <anemoia.yves@gmail.com>
2025-10-15 23:53:44 +0900

    practice: add array type example with map

--- 09.typescript/src/App.tsx
+++ 09.typescript/src/App.tsx
@@ -4,11 +4,21 @@ function App() {
     const name: string = 'yves';
     const age: number = 22;
 
+	const fruits: string[] = ['딸기', '사과', '샤인머스켓']; 
+
     return (
         <>
             <h1>헬로 월드</h1>
 			<p> 이름: {name} </p>
 			<p> 나이: {age}살 </p>
+			
+			<ul>
+				{fruits.map((fr, idx) => (
+					<li key={idx}>
+						{fr}
+					</li>
+				))}
+			</ul>
         </>
     );
 }
