b7ebb37ceb06a0e43c53896f8bed29d6b95b26ac
anemoa <anemoia.yves@gmail.com>
2025-10-16 23:57:32 +0900

    practice: add object type example

--- 09.typescript/src/App.tsx
+++ 09.typescript/src/App.tsx
@@ -6,6 +6,11 @@ function App() {
 
 	const fruits: string[] = ['딸기', '사과', '샤인머스켓']; 
 
+	const person: {name: string; age: number} = {
+		name: '도도',
+		age: 55
+	}
+
     return (
         <>
             <h1>헬로 월드</h1>
@@ -19,6 +24,12 @@ function App() {
 					</li>
 				))}
 			</ul>
+
+			<div>
+				<h2>person 정보</h2>
+				<p>이름: {person.name}</p>
+				<p>나이: {person.age}살</p>
+			</div>
         </>
     );
 }
